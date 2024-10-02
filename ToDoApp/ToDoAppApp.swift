3c7698e8d4fc55be9af4d8e8e4e07921b9adb8ef
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-02 16:37:44 +0530

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Thushini Abeysuriya on 2024-10-02.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
